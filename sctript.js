d00532a5760d1b38ba90930c10e1da80d69c921d
Resul Elezi <resul_elezi@protonmail.com>
2024-10-24 13:47:42 +0200

    #

--- sctript.js
+++ sctript.js
@@ -12,11 +12,13 @@ const decimalToBinary = (input) => {
     while(input > 0) {
         const quotient = Math.floor(input / 2);
         const remainder = input % 2;
+        
         inputs.push(input);
         quotients.push(quotient);
         remainders.push(remainder);
         input = quotient;
     }
+    console.log("Inputs: ", inputs);
 };
 
 const checkUserInput = () => {
