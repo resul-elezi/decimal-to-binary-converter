97e64d9f950d54968019f1b173869d8037118742
Resul Elezi <resul_elezi@protonmail.com>
2024-10-24 13:13:49 +0200

    #

--- sctript.js
+++ sctript.js
@@ -5,8 +5,11 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-    if (!numberInput.value || parseInt(numberInput.value)) {
-
+    if (!numberInput.value ||
+        isNaN(parseInt(numberInput.value)) ||
+        parseInt(numberInput.value)< 0) {
+        alert("Please provide a decimal number greater than or equal to 0");
+        return;
     }
 
     console.log(numberInput.value);
