c3be2b7b9a569b5982b91aca86a9118be1633e91
Resul Elezi <resul_elezi@protonmail.com>
2024-11-14 10:50:20 +0100

    #

--- sctript.js
+++ sctript.js
@@ -1,5 +1,17 @@
 "use strict";
 
+const a = () => {
+    return "freeCodeCamp " + b();
+  };
+  
+const b = () => {
+    return "is " + c();
+  };
+  
+const c = () => {
+    return "awesome!";
+  };
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
