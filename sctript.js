74c37bc3a933b092c58aae505ac093224ac6140c
Resul Elezi <resul_elezi@protonmail.com>
2024-11-28 21:17:37 +0100

    #

--- sctript.js
+++ sctript.js
@@ -6,6 +6,12 @@ const result = document.getElementById("result");
 
 const countdown = (number) => {
   console.log(number);
+
+  if(number === 0) {
+    return;
+  } else {
+    
+  }
 }
 
 const decimalToBinary = (input) => {
