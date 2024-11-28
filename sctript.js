ce48046638cae4ebbdec08442285df3a006cebc7
Resul Elezi <resul_elezi@protonmail.com>
2024-11-28 21:31:30 +0100

    #

--- sctript.js
+++ sctript.js
@@ -7,6 +7,8 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
     if(input === 0) {
       return "";
+    } else {
+      return decimalToBinary(Math.floor(input / 2));
     }
   };
 
