61551a46e4dc1e9ff50888f6771386be6cbbb167
Resul Elezi <resul_elezi@protonmail.com>
2024-11-28 21:14:05 +0100

    #

--- sctript.js
+++ sctript.js
@@ -4,6 +4,10 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const countdown = (number) => {
+  
+}
+
 const decimalToBinary = (input) => {
     let binary = "";
   
