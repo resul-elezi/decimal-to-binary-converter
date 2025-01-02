e367e899c66f92238a6b3bef33ffe84cffbdc9dc
Resul Elezi <resul_elezi@protonmail.com>
2025-01-02 18:59:24 +0100

    #

--- sctript.js
+++ sctript.js
@@ -7,16 +7,24 @@ const animationContainer = document.getElementById("animation-container");
 const animationData = [
   {
     inputVal: 5,
-    addElDelay: 1000
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
   },
   {
     inputVal: 2,
-    addElDelay: 1500
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
   },
   {
     inputVal: 1,
     addElDelay: 2000,
-    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack."
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
   }
 ];
 const decimalToBinary = (input) => {
