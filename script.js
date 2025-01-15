eacb7d8247e23388bd9f89742552ec64aee06472
Resul Elezi <resul_elezi@protonmail.com>
2025-01-15 20:34:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,16 +41,13 @@ const showAnimation = () => {
           decimalToBinary(${obj.inputVal})
         </p>`;
     }, obj.addElDelay);
-
     setTimeout(() => {
       document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
-
     setTimeout(() => {
       document.getElementById(obj.inputVal).remove();
     }, obj.removeElDelay);
   });
-
   setTimeout(() => {
 result.textContent = decimalToBinary(5);
   }, 20000);
