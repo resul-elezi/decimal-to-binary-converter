2e9d3db7dcea231d9b068e1d24728ca579968cde
Resul Elezi <resul_elezi@protonmail.com>
2025-01-15 20:35:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,20 +52,16 @@ const showAnimation = () => {
 result.textContent = decimalToBinary(5);
   }, 20000);
 };
-
 const checkUserInput = () => {
   const inputInt = parseInt(numberInput.value);
-
   if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
-
   if (inputInt === 5) {
     showAnimation();
     return;
   }
-
   result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
